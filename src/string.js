27a1d98789d56b14c239a5693585b23ca8bcbf3b
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-11-12 19:29:08 -0600

    feat: make reverseString() pass all test suites.
    Function passes all test suites given in
    string.test.js.

--- src/string.js
+++ src/string.js
@@ -3,5 +3,26 @@ const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 };
 
-export {capitalize}
+const reverseString = (string) => {
+  if(typeof string != "string") throw new TypeError("Expected a string");
+
+  if(string.length <= 1) // String is reversed
+  {
+    return string;
+  }
+
+  // Split string into two O(n log (n)) [Merge sort]
+  const middleIndex = (string.length % 2 != 0) ? Math.floor(string.length / 2) : string.length / 2 ;
+  const leftString = string.slice(0,middleIndex); 
+  const rightString = string.slice(middleIndex, string.length);
+
+  const reversedLeft = reverseString(leftString); // Reverse left
+  const reversedRight = reverseString(rightString); // Reverse Right
+
+  return reversedRight + reversedLeft; // Reverse string by making right side go before original left side of string
+
+}
+
+
+export {capitalize, reverseString}
 
